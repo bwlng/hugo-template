55d7e205af9be06ef20f16509d2972688d6a98cf
Stephen Bowling <stephen@studiosaurus.com>
2022-11-23 23:32:01 -0600

    Adds fonts and tailwind

---
+++ tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./layouts/**/*.{html,js}"],
+  theme: {
+    fontFamily: {
+      "sans": ["Gotham A", "Gotham B", "ui-sans-serif", "system-ui"],
+    },
+    extend: {},
+  },
+  plugins: [],
+}
