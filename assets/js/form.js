abf1c9897456d4eb6a85b07fd3fa994a480cddc7
Stephen Bowling <stephen@studiosaurus.com>
2023-08-20 15:32:40 -0500

    feat: add contact with form

---
+++ assets/js/form.js
@@ -0,0 +1,42 @@
+const form = document.querySelector('[data-form]');
+
+if (!form) {
+    return;
+}
+
+const onSuccess = (response) => {
+    console.log({ response })
+    const node = document.createElement('div');
+    node.classList.add('text-green-500');
+    node.innerHTML = `Thank you for your message.`;
+    form.appendChild(node);
+}
+
+const handleSubmit = event => {
+    event.preventDefault()
+
+    const formData = new FormData(form)
+    const action = form.dataset.actionUrl
+
+    // Send the form data to the API
+    fetch(action, {
+        method: "POST",
+        headers: { "Content-Type": "application/x-www-form-urlencoded" },
+        body: new URLSearchParams(formData).toString(),
+    })
+    .then(response => {
+        return response
+    })
+    .then(response => {
+        if (response.ok) {
+            onSuccess(response)
+            // Clear the form
+            form.reset()
+        }
+    })
+    .catch(error => {
+        console.error("Error:", error)
+    })
+}
+
+form.addEventListener("submit", handleSubmit)
